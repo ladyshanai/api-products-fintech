f111e43bcda6de867b0aa6dd34133fb38f1f2145
Paula <paulalezcano21@gmail.com>
2026-06-24 18:47:53 -0300

    feat(feign client): update annotation url

--- src/main/java/products/api/fintech/client/AccountClient.java
+++ src/main/java/products/api/fintech/client/AccountClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "account-service", url = "${account-service.account-service-url}")
+@FeignClient(name = "account-service")
 public interface AccountClient {
     @GetMapping("/api/v1/accounts/{id}")
     AccountModel getAccountById(@PathVariable Long id);
